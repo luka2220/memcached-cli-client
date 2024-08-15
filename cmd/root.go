909ede75e50065f712d04f6e58d3d12a80f82ca6
Luka Piplica <42144047+luka2220@users.noreply.github.com>
2024-08-15 06:31:44 -0400

    dev/cli-command init

--- cmd/root.go
+++ cmd/root.go
@@ -9,9 +9,18 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "ccmc",
 	Short: "Client for interacting with a memcached server",
-	Long:  `Memcached is a simple yet powerful server with client libraries in many programming languages. But sometimes it’s useful to be able to interact with a server from the command line. For example it’s useful to be able to use curl to fetch web pages or test/automate calls to RESTful API.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
+	Long: `Welcome to eh memcahced CLI!
+
+	Memcached is a free, open-source, high-performance, distributed memory object caching system. It is intended to speed up dynamic web applications by reducing database load. It is also an in-memory key-value store for small chunks of arbitrary data retrieved from back-end systems with higher latency. It is simple yet powerful. Its simple design promotes quick deployment, and ease of development, and solves many problems facing large data caches. Its relatively simple API is available for most popular languages. It uses a simple text-based network protocol, making it a great platform to learn how to build network clients and servers.
+
+	The CLI client communicates with memcached over a TCP connection. The support commands in this client are:
+
+	set - store some data
+	add - store some data, only if the server doesn't already hold data for this key
+	replace - store some data, only if the server already holds data for this key
+	append - Add the data to an existing key, after the existing data
+	prepend - Add the data to an exisiting key, before the existing data
+	`,
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
