5775621a1261e5fa973e27967cbad79add774e24
Luka Piplica <42144047+luka2220@users.noreply.github.com>
2024-08-19 20:17:47 -0400

    set command communicates with server

--- internal/app/client/main.go
+++ internal/app/client/main.go
@@ -3,12 +3,13 @@ package client
 import (
 	"fmt"
 	"net"
+
+	"github.com/luka2220/tools/ccmc/internal/pkg/serialization"
 )
 
 type tcpClient struct {
-	conn net.Conn
-	host string
-	port int
+	conn    net.Conn
+	address string
 }
 
 func initClient(host string, port int) (*tcpClient, error) {
@@ -29,8 +30,7 @@ func initClient(host string, port int) (*tcpClient, error) {
 
 	return &tcpClient{
 		conn,
-		host,
-		port,
+		address,
 	}, nil
 }
 
@@ -43,5 +43,25 @@ func SendSetCommand(host string, port int, key string, value string) {
 
 	defer c.conn.Close()
 
-	c.conn.Write()
+	cmd, err := serialization.SerializeCommand("set", key, 0, 0, len(value))
+	if err != nil {
+		e := fmt.Sprintf("An error occured serializing data: %v", err)
+		panic(e)
+	}
+
+	data := serialization.SerializeDataBlock(value)
+
+	_, err = c.conn.Write(cmd.Bytes())
+	if err != nil {
+		e := fmt.Sprintf("An error occured sending data to the server: %v", err)
+		panic(e)
+	}
+
+	_, err = c.conn.Write(data.Bytes())
+	if err != nil {
+		e := fmt.Sprintf("An error occured sending data to the server: %v", err)
+		panic(e)
+	}
+
+	fmt.Printf("stored key=%s, value=%s at %s", key, value, c.address)
 }
