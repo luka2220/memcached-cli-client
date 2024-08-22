26513bce36962920aa5d3b26b0274bc334124120
Luka Piplica <42144047+luka2220@users.noreply.github.com>
2024-08-22 06:35:38 -0400

    append command working with server

--- cmd/append.go
+++ cmd/append.go
@@ -1,11 +1,13 @@
 package cmd
 
 import (
+	"github.com/luka2220/tools/ccmc/internal/app/client"
 	"github.com/spf13/cobra"
 )
 
 var appendCmd = &cobra.Command{
 	Use:   "append",
+	Args:  cobra.MatchAll(cobra.ExactArgs(2)),
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -14,6 +16,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		client.SendAppendCommand(host, port, args[0], args[1])
 	},
 }
 
